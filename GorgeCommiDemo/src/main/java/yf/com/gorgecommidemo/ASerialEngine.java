3d86a031a632e73ae06a9838f7dc404c82fdda87
sujuntao <51217389@qq.com>
2017-12-06 17:58:23 +0800

    有发送数据和接收数据的UI

---
+++ GorgeCommiDemo/src/main/java/yf/com/gorgecommidemo/ASerialEngine.java
@@ -0,0 +1,143 @@
+/**
+ * ASerialEngine.java[V 1.0.0]
+ * classes: com.YF.YuanFang.YFServer.serial.rs485.ASerialEngine
+ * xuie	create 2015-4-30 ����9:40:29
+ */
+
+package yf.com.gorgecommidemo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import com.orhanobut.logger.Logger;
+import com.yf.serial.SerialPort;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class ASerialEngine extends Service implements SerialDataManager.SendData{
+    private SerialPort mSerialPort;
+    private OutputStream mOutputStream;
+    private InputStream mInputStream;
+    private ReadThread mReadThread;
+    private SendThread mSendThread;
+
+    private int BAUDRATE = 38400;
+    private boolean bReading = true;
+    private byte[] mSendBuffer = new byte[1024];
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        init();
+    }
+
+    private void init() {
+        SerialDataManager.getInstance().setSendData(this);
+        try {
+            mSerialPort = new SerialPort(new File("/dev/ttyS0"), BAUDRATE, 0);
+            mOutputStream = mSerialPort.getOutputStream();
+            mInputStream = mSerialPort.getInputStream();
+
+			/* Create a receiving thread */
+            mReadThread = new ReadThread();
+            mReadThread.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_REDELIVER_INTENT;
+    }
+
+    @Nullable @Override public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Logger.d("onDestroy!");
+        closeStream();
+    }
+
+
+    private void closeStream() {
+        try {
+            mInputStream.close();
+            mInputStream = null;
+            mOutputStream.close();
+            mOutputStream = null;
+        } catch (Exception e) {
+        }
+    }
+
+    @Override public void sendData(byte[] buffer) {
+        mSendBuffer = buffer;
+        if (mSendThread == null) {
+            mSendThread = new SendThread();
+            mSendThread.start();
+        } else {
+            if (mSendThread.isAlive()) {
+                System.out.println("mSendThread.isAlive() !!!");
+            }
+        }
+        mSendThread.run();
+    }
+
+    private class ReadThread extends Thread {
+
+        @Override
+        public void run() {
+            super.run();
+            while (bReading) {
+                try {
+                    synchronized (this) {
+                        if (mInputStream == null)
+                            return;
+                        int i = -1;
+                        while ((i = mInputStream.read()) != -1) {
+                            SerialDataManager.getInstance().receiveData((byte) i);
+                        }
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return;
+                }
+            }
+        }
+    }
+
+    private class SendThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                if (mOutputStream != null) {
+                    if (mSendBuffer != null) {
+                        byte[] buffer = mSendBuffer;
+                        mSendBuffer = null;
+
+                        System.out.print("接收数据 --> ");
+                        for (int i = 0; i < buffer.length; i++) {
+                            System.out.print((buffer[i] & 0xFF) + " ");
+                        }
+                        System.out.println();
+
+                        mOutputStream.write(buffer);
+                        mOutputStream.flush();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
--- GorgeCommiDemo/src/main/java/yf/com/gorgecommidemo/MainActivity.java
+++ GorgeCommiDemo/src/main/java/yf/com/gorgecommidemo/MainActivity.java
@@ -1,122 +1,174 @@
 package yf.com.gorgecommidemo;
 
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
-
-import com.yf.serial.RS485Service;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SerialDataManager.ReceiveData {
     private static final String TAG = "MainActivity";
 
-    RS485Service.MyBinder myBinder;
     @BindView(R.id.tv_data)
     EditText mTvData;
     @BindView(R.id.send)
     Button mSend;
-    @BindView(R.id.received_message_lv)
-    RecyclerView receivedMessageLv;
+    @BindView(R.id.received_data) TextView receivedData;
 
-    //RecyclerView布局格式
-    private LinearLayoutManager mLayoutManager;
-    //装载leftMenu的集合
-    private List<String> reMessageList=new ArrayList<String>();
-    private ReceivedAdapter receivedAdapter;
+    private SerialDataManager serialDataManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        Intent intent = new Intent(this, RS485Service.class);
-        bindService(intent, new MySerialConnection(), Context.BIND_AUTO_CREATE);
-        initListView();
+        serialDataManager = SerialDataManager.getInstance();
+        serialDataManager.setReceiveData(this);
+        startService(new Intent(this, ASerialEngine.class));
     }
 
-    class MySerialConnection implements ServiceConnection {
-
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            myBinder = (RS485Service.MyBinder) service;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-
-        }
-    }
-
-
+    /**
+     * 点击发送数据
+     */
     @OnClick(R.id.send)
     public void onViewClicked() {
-
-/*  // testInterface1();
-       // transferData(new byte[]{0x00, 0x01}, 2);
-        byte[] b = new byte[2];
-        b[0] = 0x01;
-        b[1] = 0x02;
-        Log.i(TAG,"myBinder = "+myBinder);
-//        Log.i(TAG,"myBinder = "+myBinder);
-        myBinder.getISSend().transferData(b, b.length);*/
-
-
-        // testInterface1();
-        // transferData(new byte[]{0x00, 0x01}, 2);
-//        byte[] b = new byte[1024];
-//       String[] str=mTvData.getText().toString().split("\\s+");
-//      for (int i=0;i<=str.length;i++){
-//          b[i]= Byte.parseByte(str[i]);
-//      }
-        //    Log.i(TAG,b+"");
-//        for(String ss : arr){
-//            System.out.println(ss);
-//        }
         byte[] b = new byte[7];
-        b[0] = 0x01;
-        b[1] = 0x02;
-        b[0] = (byte) 0xAA;
-        b[1] = (byte) 0xDD;
-        b[2] = 0x03;
-        b[3] = 0x01;
-        b[4] = 0x2D;
-        b[5] = 0x01;
-        b[6] = (byte) 0xB9;
-//        b[0] = 0x1E;
-//        b[1] = 0x02;
-        Log.i(TAG, "myBinder =======> " + myBinder);
-//        Log.i(TAG,"myBinder = "+myBinder);
-        // 0xAA, 0xDD, 0x03, 0x01, 0x2D, 0x01 0xB9
+        String[] str = mTvData.getText().toString().split("\\s+");
+        Log.i(TAG, "====>" + str.length);
 
-        myBinder.getISSend().transferData(b, b.length);
+            try {
+                int a = getI(String.valueOf(str[0].toLowerCase().charAt(0))) * 16 + getI(String.valueOf(str[0].charAt(1)));
+                int c = getI(String.valueOf(str[1].toLowerCase().charAt(0))) * 16 + getI(String.valueOf(str[1].charAt(1)));
+                b[0] = (byte) 0xAA;
+                b[1] = (byte) 0xDD;
+                b[2] = 0x03;
+                b[3] = 0x01;
+                b[4] = (byte) a;
+                b[5] = (byte) c;
+                b[6] = (byte) 0xB9;
+
+                serialDataManager.sendData(b);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(this, "对不起,请输入两个byte数据", Toast.LENGTH_SHORT).show();
+
+            }
 
     }
 
-    private void initListView(){
-        mLayoutManager = new LinearLayoutManager(this);
-        receivedMessageLv.setLayoutManager(mLayoutManager);
-        //如果可以确定每个item的高度是固定的，设置这个选项可以提高性能
-        receivedMessageLv.setHasFixedSize(true);
-        //为RecyclerView添加默认动画效果
-        receivedMessageLv.setItemAnimator(new DefaultItemAnimator());
-        reMessageList.add(myBinder.getReceivedData());
-        receivedAdapter=new ReceivedAdapter(reMessageList,this);
-        receivedMessageLv.setAdapter(receivedAdapter);
+    @Override public void receiveData(byte buffer) {
+        Log.e(TAG, "receiveData");
+        combiner(buffer);
+    }
+
+    private int getI(String a) {
+        int b = 0;
+        switch (a) {
+            case "0":
+                b = 0;
+                break;
+            case "1":
+                b = 1;
+                break;
+            case "2":
+                b = 2;
+                break;
+            case "3":
+                b = 3;
+                break;
+            case "4":
+                b = 4;
+                break;
+            case "5":
+                b = 5;
+                break;
+            case "6":
+                b = 6;
+                break;
+            case "7":
+                b = 7;
+                break;
+            case "8":
+                b = 8;
+                break;
+            case "9":
+                b = 9;
+                break;
+            case "a":
+                b = 10;
+                break;
+            case "b":
+                b = 11;
+                break;
+            case "c":
+                b = 12;
+                break;
+            case "d":
+                b = 13;
+                break;
+            case "e":
+                b = 14;
+                break;
+            case "f":
+                b = 15;
+                break;
+        }
+        return b;
+    }
+
+    /**
+     * bytes转换成十六进制字符串
+     *
+     * @param b byte数组
+     * @return String 每个Byte值之间空格分隔
+     */
+    public static String byte2HexStr(byte[] b) {
+        String stmp = "";
+        StringBuilder sb = new StringBuilder("");
+        for (int n = 0; n < b.length; n++) {
+            stmp = Integer.toHexString(b[n] & 0xFF);
+            sb.append((stmp.length() == 1) ? "0" + stmp : stmp);
+            sb.append(" ");
+        }
+        return sb.toString().toUpperCase().trim();
+    }
+
+    private int pos = 0;
+    private boolean dataStartCombiner = false;
+    private byte[] buffer = new byte[64];
+
+    public void combiner(byte b) {
+        if (b == (byte) 0xFA && pos == 0) {
+            Log.e(TAG, "combiner");
+            dataStartCombiner = true;
+        }
+        if (dataStartCombiner) {
+            if (pos <= 64) {
+                buffer[pos] = b;
+                ++pos;
+            }
+        }
+        if (pos == 8) {
+            pos = 0;
+            dataStartCombiner = false;
+            byte[] buff = new byte[8];
+            System.arraycopy(buffer, 0, buff, 0, 8);
+            final String a = byte2HexStr(buff);
+            Log.e(TAG, "a = " + a);
+            runOnUiThread(new Runnable() {
+                @Override public void run() {
+                    CharSequence str = receivedData.getText();
+                    receivedData.setText(str + a + "\n");
+                }
+            });
+        }
     }
 
 }
